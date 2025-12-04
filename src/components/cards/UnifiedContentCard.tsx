1d566af02fe8bb962979223f08637b38865ea073
stenkjan <deine-email@example.com>
2025-12-04 16:27:40 +0100

    Update eslint-config-next to version 15.3.6 and modify isCritical prop in UnifiedContentCard to false for improved functionality and consistency

--- src/components/cards/UnifiedContentCard.tsx
+++ src/components/cards/UnifiedContentCard.tsx
@@ -1657,7 +1657,7 @@ export default function UnifiedContentCard({
                   height={140}
                   className="w-full h-auto object-contain object-left"
                   strategy="client"
-                  isCritical={true}
+                  isCritical={false}
                   enableCache={true}
                 />
               </div>
